0bafb8e831028a417340669c4522e48890378221
Dhiransh Saxena <dhiranshsaxena@gmail.com>
2020-11-11 04:23:26 +0530

    Beta Version

---
+++ lib/Circulars/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+const appOrange = const Color(0xFFFB8C00);
+const primaryAppColor= Colors.black12;
+
+const primaryColor = const Color(0xFFFF5252);
+const primaryLight = const Color(0xFFFF5252);
+const primaryDark = const Color(0xFFFF5252);
+
+const secondaryColor = const Color(0xFFFF5252);
+const secondaryLight = const Color(0xFFFF5252);
+const secondaryDark = const Color(0xFFFF5252);
+
+const Color gradientStart = const Color(0xFF485563);
+const Color gradientEnd = const Color(0xFF29323C);
---
+++ lib/PreviousPapers/schools.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:lpchub/PreviousPapers/cms.dart';
+
+import 'lpc.dart';
+
+class Schools extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff104976),
+        elevation: 10.0,
+        title: Text(
+            "Select Your School",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'MeriendaOne'
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Card(
+                elevation: 10.0,
+                color: Color(0xff104976),
+                child: Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width-40,
+                  child: ListTile(
+                    onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => LPC(),));
+                    },
+                    title: Center(
+                        child: Text(
+                            "Lucknow Public College",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'MeriendaOne',
+                            color: Color(0xffE69494),
+                            fontSize: 28
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Card(
+                elevation: 10.0,
+                color: Color(0xff104976),
+                child: Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width-40,
+                  child: ListTile(
+                    onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => CMS(),));
+                    },
+                    title: Center(
+                        child: Text(
+                          "City Montessori School",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'MeriendaOne',
+                              color: Color(0xffE69494),
+                              fontSize: 28
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
